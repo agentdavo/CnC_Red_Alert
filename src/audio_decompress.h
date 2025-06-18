a2702e84b4e50923bc7bc9cf734315971c885e16
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-18 14:14:38 +0100

    Document audio decompression wrappers

---
+++ src/audio_decompress.h
@@ -0,0 +1,19 @@
+#ifndef AUDIO_DECOMPRESS_H
+#define AUDIO_DECOMPRESS_H
+
+#include "WIN32LIB/INCLUDE/soscomp.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+long aud_decompress_frame(void *source, void *dest, long size);
+void aud_sos_init_stream(_SOS_COMPRESS_INFO *info);
+unsigned long aud_sos_decompress(_SOS_COMPRESS_INFO *info, unsigned long bytes);
+unsigned long aud_sos_decompress_general(_SOS_COMPRESS_INFO *info, unsigned long bytes);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AUDIO_DECOMPRESS_H */
